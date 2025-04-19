bf24c3d73365ae4d3f8aff698cb8526ba8bdf740
Andrej Ambrož <93589920+AmbyCe@users.noreply.github.com>
2025-04-19 11:02:54 +0200

    Adding new student

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
+import Models.Classes.CybersecurityStudent;
 import Models.Classes.Student;
+import Models.Classes.TelecommunicationsStudent;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    private static HashMap<Integer, Student> students = new HashMap<>();
+
     public static void main(String[] args) {
         System.out.println("#################### DATABAZE STUDENTU ####################");
         System.out.println("#  (Pokracujte vyberem moznosti zadanim cisla do vstupu)  #");
         System.out.println("# ------------------------------------------------------- #");
         System.out.println("# 1) Prijmout studenta na univerzitu (pridat studenta)    #");
+        System.out.println("# 2) Zadat studentovi novou znamku                        #");
         System.out.println("#################### DATABAZE STUDENTU ####################");
 
-        ArrayList<Student> students = new ArrayList<>();
-
-        System.out.print("[#] Zadejte moznost: ");
+        System.out.print("[>] Zadejte moznost: ");
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.next();
         int chosenOption;
@@ -24,6 +26,10 @@ public class Main {
         catch (NumberFormatException e)
         {
             System.out.println("[!] CHYBA! Vybrana moznost musi byt cislo!");
+
+            System.out.println("[#] Pokracujte stisknutim jakekoliv klavesy...");
+            scanner.nextLine();
+            scanner.nextLine();
             main(null);
             return;
         }
@@ -31,18 +37,44 @@ public class Main {
         switch (chosenOption)
         {
             case 1:
+                System.out.println("");
+                System.out.println("--->   Moznost 1: Pridani noveho studenta na univerzitu   <---");
+
+                String studentGroup;
                 String name;
                 String surname;
                 String dateOfBirth;
 
-                System.out.print("[1/3] Zadej krestni jmeno studenta: ");
+                System.out.print("[1/4] Je to student (K)yberbezpecnosti nebo (T)elekomunikaci? ");
+                studentGroup = scanner.next();
+                if (!Objects.equals(studentGroup, "K") && !Objects.equals(studentGroup, "T"))
+                {
+                    System.out.println("[!] CHYBA! Obor studia musi byt K nebo T!");
+
+                    System.out.println("[#] Pokracujte stisknutim jakekoliv klavesy...");
+                    scanner.nextLine();
+                    scanner.nextLine();
+                    main(null);
+                    return;
+                }
+                System.out.print("[2/4] Zadejte krestni jmeno studenta: ");
                 name = scanner.next();
-                System.out.print("[2/3] Zadej prijmeni studenta: ");
+                System.out.print("[3/4] Zadejte prijmeni studenta: ");
                 surname = scanner.next();
-                System.out.print("[3/3] Zadej datum narozeni studenta (DD.MM.YYYY): ");
+                System.out.print("[4/4] Zadejte datum narozeni studenta (DD.MM.YYYY): ");
                 dateOfBirth = scanner.next();
 
-                students.add(new Student(students.size() + 1, name, surname, dateOfBirth));
+                if (studentGroup.equals("K"))
+                    students.put(students.size() + 1, new CybersecurityStudent(students.size() + 1, name, surname, dateOfBirth));
+                else
+                    students.put(students.size() + 1, new TelecommunicationsStudent(students.size() + 1, name, surname, dateOfBirth));
+                Student lastAddedStudent = students.get(students.size());
+                System.out.println("[>] USPECH! Byl pridan student: (#" + lastAddedStudent.getId() + ") " + lastAddedStudent.getName() + " " + lastAddedStudent.getSurname());
+
+                System.out.println("[#] Pokracujte stisknutim jakekoliv klavesy...");
+                scanner.nextLine();
+                scanner.nextLine();
+                main(null);
                 break;
         }
     }
